15496f0ba40a3a2bb83772fe3f1c6d0843e88562
Marina Horuzhaya <marinakh192@gmail.com>
2023-08-02 00:08:04 +0300

    openEnterprise method added

---
+++ src/test/java/EnterpriseTest.java
@@ -0,0 +1,26 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class EnterpriseTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy="eager";
+    }
+
+    @Test
+    void openEnterprise() {
+        open("https://github.com/");
+        $(".header-menu-wrapper").$(Selectors.byText("Solutions")).hover();
+        $(".border-bottom").$(Selectors.byText("Enterprise")).click();
+        $(".application-main ").shouldHave(Condition.text("Build like the best"));
+    }
+}
